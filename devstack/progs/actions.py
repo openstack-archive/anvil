06fea839982fcfa119b04f6c1a9d08c2754804ef
Joshua Harlow <harlowja@gmail.com>
2012-01-22 16:51:12 -0800

    Some code shuffling.

--- devstack/progs/actions.py
+++ devstack/progs/actions.py
@@ -333,18 +333,17 @@ def _run_action(args):
                 components[new_component] = list()
     #get the right component order (by priority)
     component_order = settings.prioritize_components(components.keys())
-    #now do it!
-    LOG.info("Starting action [%s] on %s for distro [%s]" % (action, date.rcf8222date(), distro))
-    #add in any that will just be referenced but which will not actually do anything
+    #add in any that will just be referenced but which will not actually do anything (ie the action will not be applied to these)
     ref_components = settings.parse_components(args.pop("ref_components"))
     for c in ref_components.keys():
         if(c not in components):
             components[c] = ref_components.get(c)
+    #now do it!
+    LOG.info("Starting action [%s] on %s for distro [%s]" % (action, date.rcf8222date(), distro))
     results = _run_components(action, component_order, components, distro, rootdir, args)
     LOG.info("Finished action [%s] on %s" % (action, date.rcf8222date()))
-    if(results and len(results)):
-        msg = 'Check [%s] for traces of what happened.' % ", ".join(results)
-        LOG.info(msg)
+    if(results):
+        LOG.info(Check [%s] for traces of what happened.' % ", ".join(results))
     return True
 
 
