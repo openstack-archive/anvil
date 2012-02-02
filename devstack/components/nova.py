f1e3410ce62d2826c2cdc499fd071ce40f8d6b7d
Ken Thomas <krt@yahoo-inc.com>
2012-02-02 13:44:59 -0800

    Fixed using wrong method

--- devstack/components/nova.py
+++ devstack/components/nova.py
@@ -588,11 +588,11 @@ class NovaConfigurator(object):
             nova_conf.add('xenapi_connection_password', self.cfg.get("passwords", "xenapi_connection"))
             nova_conf.add_simple('noflat_injected')
             nova_conf.add('flat_interface', 'eth1')
-            nova_conf.add('firewall_driver', self.cfg.getstr('xen_firewall_driver'))
+            nova_conf.add('firewall_driver', self._getstr('xen_firewall_driver'))
             nova_conf.add('flat_network_bridge', 'xapi1')
         else:
             nova_conf.add('connection_type', 'libvirt')
-            nova_conf.add('firewall_driver', self.cfg.getstr('libvirt_firewall_driver'))
+            nova_conf.add('firewall_driver', self._getstr('libvirt_firewall_driver'))
             nova_conf.add('flat_network_bridge', self._getstr('flat_network_bridge'))
             flat_interface = self._getstr('flat_interface')
             if flat_interface:
