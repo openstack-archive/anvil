95966639036a0538071480b9b5fdb599acaf418a
Ken Thomas <krt@yahoo-inc.com>
2012-02-07 16:08:54 -0800

    Script to clean up iptables as part of nova uninstall

---
+++ utils/clean_iptables.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This script cleans up the iptables as part of a nova uninstall
+
+# Delete rules
+iptables -S -v | sed "s/-c [0-9]* [0-9]* //g" | grep "nova" | grep "\-A" |  sed "s/-A/-D/g" | awk '{print "sudo iptables",$0}' | bash
+# Delete nat rules
+iptables -S -v -t nat | sed "s/-c [0-9]* [0-9]* //g" | grep "nova" |  grep "\-A" | sed "s/-A/-D/g" | awk '{print "sudo iptables -t nat",$0}' | bash
+# Delete chains
+iptables -S -v | sed "s/-c [0-9]* [0-9]* //g" | grep "nova" | grep "\-N" |  sed "s/-N/-X/g" | awk '{print "sudo iptables",$0}' | bash
+# Delete nat chains
+iptables -S -v -t nat | sed "s/-c [0-9]* [0-9]* //g" | grep "nova" |  grep "\-N" | sed "s/-N/-X/g" | awk '{print "sudo iptables -t nat",$0}' | bash
