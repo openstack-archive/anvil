0ba4a56e8e578af4315a9634f14045aff267d38f
Joshua Harlow <harlowja@yahoo-inc.com>
2012-02-13 16:38:01 -0800

    Fixing naming.

--- run_tests.py
+++ run_tests.py
@@ -30,28 +30,28 @@ def parse_suite_filter():
     """ Parses out -O or --only argument and returns the value after it as the
     filter. Removes it from sys.argv in the process. """
 
-    oursuitefilter = None
+    suitefilter = None
     if '-O' in sys.argv or '--only' in sys.argv:
         for i in range(len(sys.argv)):
             if sys.argv[i] in ['-O', '--only']:
                 if len(sys.argv) > i + 1:
                     # Remove -O/--only settings from sys.argv
                     sys.argv.pop(i)
-                    oursuitefilter = sys.argv.pop(i)
+                    suitefilter = sys.argv.pop(i)
                     break
-    return oursuitefilter
+    return suitefilter
 
 
 if __name__ == '__main__':
-    suite_filter = parse_suite_filter()
-    if suite_filter:
+    SUITE_FILTER = parse_suite_filter()
+    if SUITE_FILTER:
         TESTS = [t for t in TESTS if filter in str(t)]
         if not TESTS:
-            print 'No test configuration by the name %s found' % filter
+            print 'No test configuration by the name %s found' % SUITE_FILTER
             sys.exit(2)
     #Run test suites
     if len(TESTS) > 1:
-        cwddirectory = os.getcwd()
+        CWD_DIRECTORY = os.getcwd()
         for test_num, test_cls in enumerate(TESTS):
             try:
                 result = test_cls().run()
@@ -65,7 +65,7 @@ if __name__ == '__main__':
                 sys.exit(1)
             # Collect coverage from each run. They'll be combined later in .sh
             if '--with-coverage' in sys.argv:
-                coverage_file = os.path.join(cwddirectory, ".coverage")
+                coverage_file = os.path.join(CWD_DIRECTORY, ".coverage")
                 target_file = "%s.%s" % (coverage_file, test_cls.__name__)
                 try:
                     if os.path.exists(target_file):
