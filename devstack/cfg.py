831d8c54f264eec02564ac83d3ae085b2461f0a8
Joshua Harlow <harlowja@yahoo-inc.com>
2012-02-13 17:01:44 -0800

    Removing unused import.

--- devstack/cfg.py
+++ devstack/cfg.py
@@ -21,7 +21,6 @@ from devstack import date
 from devstack import env
 from devstack import exceptions as excp
 from devstack import log as logging
-from devstack import settings
 from devstack import shell as sh
 from devstack import utils
 
