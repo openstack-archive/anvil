ae48d1433c622377c7a178c1e01439a44f5edf86
Joshua Harlow <harlowja@yahoo-inc.com>
2012-02-14 17:47:32 -0800

    Taking a little bit of what the openstack keystone guys have done for logging that might be useful in the future.

--- devstack/log.py
+++ devstack/log.py
@@ -2,6 +2,9 @@
 
 #    Copyright (C) 2012 Yahoo! Inc. All Rights Reserved.
 #
+#    Copyright 2011 OpenStack LLC.
+#    All Rights Reserved.
+#
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
@@ -14,13 +17,50 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import functools
 import logging
+import pprint
 
-#
-#TODO remove this whole file (??)
-#
+from logging.handlers import SysLogHandler
+from logging.handlers import WatchedFileHandler
+
+# A list of things we want to replicate from logging levels
+CRITICAL = logging.CRITICAL
+FATAL = logging.FATAL
+ERROR = logging.ERROR
+WARNING = logging.WARNING
+WARN = logging.WARN
+INFO = logging.INFO
+DEBUG = logging.DEBUG
+NOTSET = logging.NOTSET
 
 # methods
 getLogger = logging.getLogger
+debug = logging.debug
+info = logging.info
+warning = logging.warning
+warn = logging.warn
+error = logging.error
+exception = logging.exception
+critical = logging.critical
+log = logging.log
+
+# classes
+root = logging.root
+Formatter = logging.Formatter
+
+# handlers
+StreamHandler = logging.StreamHandler
+WatchedFileHandler = WatchedFileHandler
+SysLogHandler = SysLogHandler
 
 
+def log_debug(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kw):
+        logging.debug('%s(%s, %s) ->', f.func_name, str(args), str(kw))
+        rv = f(*args, **kw)
+        logging.debug(pprint.pformat(rv, indent=2))
+        logging.debug('')
+        return rv
+    return wrapper
