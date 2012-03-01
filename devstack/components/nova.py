a1b1b38af00e3681514d0301287f446eee585c92
Joshua Harlow <harlowja@yahoo-inc.com>
2012-03-01 13:59:50 -0800

    Added message for nova quantum configuration.

--- devstack/components/nova.py
+++ devstack/components/nova.py
@@ -783,6 +783,7 @@ class NovaConfConfigurator(object):
         nova_conf.add('iscsi_help', 'tgtadm')
 
     def _configure_network_settings(self, nova_conf):
+        #TODO this might not be right.... 
         if utils.service_enabled(settings.QUANTUM, self.instances, False):
             nova_conf.add('network_manager', QUANTUM_MANAGER)
             nova_conf.add('quantum_connection_host', self.cfg.get('quantum', 'q_host'))
