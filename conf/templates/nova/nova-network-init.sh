fdd255052067d54486ecadb92a600b2915f7c5f0
Joshua Harlow <harlowja@yahoo-inc.com>
2012-03-26 22:44:05 -0700

    More variable name fixes

--- conf/templates/nova/nova-network-init.sh
+++ conf/templates/nova/nova-network-init.sh
@@ -4,15 +4,15 @@ set -o xtrace
 set -o errexit
 
 # Create a small network
-%NOVA_DIR%/bin/nova-manage --flagfile %CFG_FILE% network create private %FIXED_RANGE% 1 %FIXED_NETWORK_SIZE%
+%BIN_DIR%/nova-manage --flagfile %CFG_FILE% network create private %FIXED_RANGE% 1 %FIXED_NETWORK_SIZE%
 
-if [[ "$ENABLED_SERVICES" =~ "quantum-server" ]]; then
+if [[ "$ENABLED_SERVICES" =~ "quantum" ]]; then
     echo "Not creating floating IPs (not supported by Quantum)"
 else
     # Create some floating ips
-    %NOVA_DIR%/bin/nova-manage --flagfile %CFG_FILE% floating create %FLOATING_RANGE%
+    %BIN_DIR%/nova-manage --flagfile %CFG_FILE% floating create %FLOATING_RANGE%
 
     # Create a second pool
-    %NOVA_DIR%/bin/nova-manage --flagfile %CFG_FILE% floating create --ip_range=%TEST_FLOATING_RANGE% --pool=%TEST_FLOATING_POOL%
+    %BIN_DIR%/nova-manage --flagfile %CFG_FILE% floating create --ip_range=%TEST_FLOATING_RANGE% --pool=%TEST_FLOATING_POOL%
 fi
 
