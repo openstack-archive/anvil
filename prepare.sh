33ee0fa54f7bf2aff930db94714c9a1752aead95
Joshua Harlow <harlowja@gmail.com>
2012-03-27 22:09:39 -0700

    Fixed prepare requring yes for yum

--- prepare.sh
+++ prepare.sh
@@ -26,7 +26,7 @@ elif [[ `cat /etc/issue | grep -i "red hat enterprise.*release.*6.*"` ]] ; then
     PKGS="python-pip gcc python-netifaces git python-pep8 pylint python-progressbar python"
     PIPS="termcolor pyyaml"
     PIP="pip-python -q"
-    YUM="yum install -q"
+    YUM="yum install -q -y"
     WGET="wget -q"
     # Now do it!
     echo "Preparing DEVSTACKpy for RHEL 6"
@@ -44,7 +44,7 @@ elif [[ `cat /etc/issue | grep -i "fedora.*release.*16"` ]] ; then
     PKGS="python-pip gcc python-netifaces git python-pep8 pylint python-yaml python python-progressbar"
     PIPS="termcolor"
     PIP="pip-python -q"
-    YUM="yum install -q"
+    YUM="yum install -q -y"
     # Now do it!
     echo "Preparing DEVSTACKpy for Fedora 16"
     echo "Installing packages: $PKGS"
