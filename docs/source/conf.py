9851a55ff06e2ebdb53aa1a74cc2d6e5db3f70ac
Joshua Harlow <harlowja@yahoo-inc.com>
2012-04-07 09:54:55 -0700

    Update to new transparent logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -109,7 +109,7 @@ html_title = "DEVSTACKpy"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'img/logo.jpg'
+html_logo = 'img/logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
