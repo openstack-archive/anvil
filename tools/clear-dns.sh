0343aed23d22c5d0cd3b3af7df438bce1ad90984
Joshua Harlow <harlowja@yahoo-inc.com>
2012-04-26 15:48:04 -0700

    Add clear dns util

---
+++ tools/clear-dns.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+echo "Clearing your dns"
+
+for pid in `ps -elf | grep -i dnsmasq | grep nova | perl -le 'while (<>) { my $pid = (split /\s+/)[3]; print $pid; }'`
+do
+    echo "Killing leftover nova dnsmasq process with process id $pid"
+    kill -9 $pid
+done
+
