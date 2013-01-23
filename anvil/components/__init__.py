8a7b287d3547543d0d926de530eaaa70072fb134
Nikita Savin <nsavin@griddynamics.com>
2013-01-22 23:14:40 -0800

    * run all tests, bail out with list of failed tests if any

--- anvil/components/__init__.py
+++ anvil/components/__init__.py
@@ -865,9 +865,12 @@ class PythonTestingComponent(component.Component):
             return
         cmd = self._get_test_command()
         env = self._get_env()
-        sh.execute(*cmd, stdout_fh=None, stderr_fh=None, cwd=app_dir, env_overrides=env)
-
-
+        try:
+            sh.execute(*cmd, stdout_fh=None, stderr_fh=None, cwd=app_dir, env_overrides=env)
+        except excp.ProcessExecutionError as e:
+            return e.exit_code
+        # test passed
+        return 0
 #### 
 #### PACKAGING CLASSES
 ####
