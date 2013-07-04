59cefe693cf40ecefb620ac47cf3acc6c61570e0
Joshua Harlow <harlowja@yahoo-inc.com>
2013-07-04 16:05:11 -0700

    Use a gzip helper function to do gzip actions.
    Instead of having a shell execute to do gzip and
    a python gzip function just switch to using a single
    helper gzip python function for both.
    
    Change-Id: Ie8e56f0e1f18fc32fdc2db9f3f3f91eda335308f

--- anvil/packaging/yum.py
+++ anvil/packaging/yum.py
@@ -20,7 +20,6 @@ import os
 import pkg_resources
 import sys
 
-import gzip
 import rpm
 import tarfile
 
@@ -489,8 +488,8 @@ class YumDependencyHandler(base.DependencyHandler):
             "HEAD",
         ]
         sh.execute(cmdline, cwd=pkg_dir)
-        cmdline = ["gzip", output_filename]
-        sh.execute(cmdline)
+        sh.gzip(output_filename)
+        sh.unlink(output_filename)
 
     def _write_python_tarball(self, instance, pkg_dir, ensure_exists=None):
 
@@ -529,10 +528,7 @@ class YumDependencyHandler(base.DependencyHandler):
                 with contextlib.closing(tarfile.open(archive_name, 'a')) as tfh:
                     for (tar_path, source_path) in missing_paths.items():
                         tfh.add(source_path, tar_path)
-        gz_archive_name = "%s.gz" % (archive_name)
-        with contextlib.closing(gzip.open(gz_archive_name, 'wb')) as tz:
-            with open(archive_name, 'rb') as fh:
-                tz.write(fh.read())
+        sh.gzip(archive_name)
         sh.unlink(archive_name)
 
     @staticmethod
