49550e94d5c79b627a1de3c2bbb2d9c0ffe62832
Ivan A. Melnikov <imelnikov@griddynamics.com>
2013-08-29 12:38:16 +0400

    Always create directories when moving RPMs
    Even if there are no RPMs to move and even source directory does
    not exist, we still need to create target directory because
    we rely on its existence later.
    
    Closes-Bug: #1218229
    Change-Id: I382794ab6455f045cbd8d8b3a1b2919fadee983f

--- anvil/packaging/yum.py
+++ anvil/packaging/yum.py
@@ -161,10 +161,12 @@ class YumDependencyHandler(base.DependencyHandler):
                     self._move_srpms("anvil")
 
     def _move_rpm_files(self, source_dir, target_dir):
-        if not sh.isdir(source_dir):
-            return
+        # NOTE(imelnikov): we should create target_dir even if we have
+        #  nothing to move, because later we rely on its existence
         if not sh.isdir(target_dir):
             sh.mkdirslist(target_dir, tracewriter=self.tracewriter)
+        if not sh.isdir(source_dir):
+            return
         for filename in sh.listdir(source_dir, recursive=True, files_only=True):
             if not filename.lower().endswith(".rpm"):
                 continue
