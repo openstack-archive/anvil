9a4ef16fc0131ab27ef3d202f8ea495595ada108
Dmitry <dkhovyakov@griddynamics.com>
2014-01-23 12:47:09 +0200

    yyoom tool accepts new parameters --log-file and --quiet
    Changed command prompt parameters for yyoom tool.
    There are two new parameters:
    --log-file to set path to log file
    --quiet write only error information in err
    yum_helper now calls execute istead of execute_save_output2
    
    Change-Id: I4e5583a902d2243e99b34664055b5e65be83868d

--- anvil/packaging/helpers/yum_helper.py
+++ anvil/packaging/helpers/yum_helper.py
@@ -50,11 +50,14 @@ class Helper(object):
         self._available = None
 
     def _yyoom(self, arglist, cmd_type):
-        cmdline = [self.yyoom_executable, '--verbose']
-        cmdline.extend(arglist)
+        # Just check that directory where we shall write logs actually exist
+        if self._log_dir:
+            # And create it if not
+            sh.mkdir(self._log_dir, recurse=True)
         out_filename = sh.joinpths(self._log_dir, "yyoom-%s.log" % (cmd_type))
-        (stdout, _) = sh.execute_save_output2(cmdline,
-                                              stderr_filename=out_filename)
+        cmdline = [self.yyoom_executable, '--verbose', '--log-file', out_filename, '--quiet']
+        cmdline.extend(arglist)
+        (stdout, _) = sh.execute(cmdline, stderr_fh=sys.stderr)
         return _parse_json(stdout)
 
     def _traced_yyoom(self, arglist, cmd_type, tracewriter):
