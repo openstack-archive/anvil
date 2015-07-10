07414422c9c97a27d5d3efa3d9ee56daaeb06e65
Joshua Harlow <harlowja@yahoo-inc.com>
2015-07-10 11:08:46 -0700

    Avoid calling into pip apis to get/run egg_info commands
    Change-Id: I74e3c48b9a818ab11345436210f03b9a633d9d2b

--- anvil/packaging/helpers/pip_helper.py
+++ anvil/packaging/helpers/pip_helper.py
@@ -17,10 +17,12 @@
 from distutils import version as dist_version
 import pkg_resources
 import re
+import sys
 import tempfile
 import threading
 
 from pip import req as pip_req
+import pkginfo
 
 try:
     from pip import util as pip_util
@@ -108,29 +110,25 @@ def get_directory_details(path):
         if cache_key in EGGS_DETAILED:
             return EGGS_DETAILED[cache_key]
 
-        req = extract(path)
-        req.source_dir = path
-        req.run_egg_info()
+        cmd = [sys.executable, 'setup.py', 'egg_info']
+        sh.execute(cmd, cwd=path)
+        details = pkginfo.get_metadata(path)
+        if not details:
+            raise RuntimeError("No egg detail information discovered"
+                               " at '%s'" % path)
 
-        dependencies = []
-        for d in req.requirements():
-            if not d.startswith("-e") and d.find("#"):
-                d = d.split("#")[0]
-            d = d.strip()
-            if d:
-                dependencies.append(d)
-
-        details = {
-            'req': req.req,
-            'dependencies': dependencies,
-            'name': req.name,
-            'pkg_info': req.pkg_info(),
-            'dependency_links': req.dependency_links,
-            'version': req.installed_version,
+        egg_details = {
+            'req': create_requirement(details.name, version=details.version),
         }
+        for attr_name in ['description', 'author',
+                          'version', 'name', 'summary']:
+            egg_details[attr_name] = getattr(details, attr_name)
 
-        EGGS_DETAILED[cache_key] = details
-        return details
+        LOG.debug("Extracted '%s' egg detail information:", path)
+        utils.log_object(egg_details, logger=LOG, level=logging.DEBUG)
+
+        EGGS_DETAILED[cache_key] = egg_details
+        return egg_details
 
 
 def drop_caches():
