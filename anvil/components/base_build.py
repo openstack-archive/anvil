e351b3caf6eb88e3fc096b68175c952ed8f798ab
Joshua Harlow <harlowja@yahoo-inc.com>
2015-07-13 14:26:32 -0700

    Allow using simple interpolated keys as options (and use it)
    Change-Id: Icc783753f6913a4c86b6560f8c8e68fd7367b600

--- anvil/components/base_build.py
+++ anvil/components/base_build.py
@@ -40,6 +40,7 @@ class BuildComponent(base.BasicComponent):
 
 
 class PythonBuildComponent(BuildComponent):
+
     def __init__(self, *args, **kargs):
         super(PythonBuildComponent, self).__init__(*args, **kargs)
         self._origins_fn = kargs['origins_fn']
@@ -84,7 +85,7 @@ class PythonBuildComponent(BuildComponent):
     @property
     def egg_info(self):
         app_dir = self.get_option('app_dir')
-        pbr_version = self.get_option('pbr_version')
+        pbr_version = self.get_interpolated_option("pbr_version")
         egg_info = pip_helper.get_directory_details(app_dir, pbr_version=pbr_version)
         egg_info = egg_info.copy()
         egg_info['dependencies'] = pip_helper.read_requirement_files(self.requires_files)[1]
